38f6a5c55f6e09b76d25668a01e192994aea9a92
ansh mittal <anshmittal280706@gmail.com>
2026-08-14 11:31:52 +0530

    added backend folder

---
+++ backend/src/main/java/com/pulseOps/Backend/BackendApplication.java
@@ -0,0 +1,17 @@
+package com.pulseOps.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+	public static void main(String[] args) {
+		System.out.print("hello world");
+
+		SpringApplication.run(BackendApplication.class, args);
+
+
+	}
+
+
+}
